00e4dd2c9f497d3d8ce2f499b23339a43b790959
Tiago <tiagobnarita@gmail.com>
2024-02-10 13:58:19 -0300

    add other numbers

--- index.js
+++ index.js
@@ -1,23 +1,73 @@
-var numberDisplay = 0;
+var numberDisplay = "";
 
-const zero = document.getElementById("zero");
 const display = document.getElementById("display");
+const clear = document.getElementById("clear");
+const zero = document.getElementById("zero");
 const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
 
 //inicialize buttons
+clear.onclick = buttonClear;
 zero.onclick = buttonZero;
 one.onclick = buttonOne;
+two.onclick = buttonTwo;
+three.onclick = buttonThree;
+four.onclick = buttonFour;
+five.onclick = buttonFive;
+six.onclick = buttonSix;
+seven.onclick = buttonSeven;
+eight.onclick = buttonEight;
+nine.onclick = buttonNine;
 
-function buttonOne() {
-    numberDisplay = 1;
-    display.innerText = numberDisplay;
+function buttonClear() {
+    numberDisplay = "";
+    display.innerHTML = "0";
 }
 
-function buttonZero() { 
-    console.log(display.innerHTML );
-    
-    if(display.innerHTML != 0)
-    numberDisplay += "0";
-    console.log(numberDisplay);
-    display.innerText = numberDisplay;
+function buttonZero() {
+    if (display.innerHTML != 0) {
+        numberDisplay += "0";
+        display.innerHTML = numberDisplay;
+    }
+}
+function buttonOne() {
+    addNumber(1);
+}
+
+function buttonTwo() {
+    addNumber(2);
+}
+function buttonThree() {
+    addNumber(3);
+}
+function buttonFour() {
+    addNumber(4);
+}
+function buttonFive() {
+    addNumber(5);
+}
+function buttonSix() {
+    addNumber(6);
+}
+function buttonSeven() {
+    addNumber(7);
+}
+function buttonEight() {
+    addNumber(8);
+}
+function buttonNine() {
+    addNumber(9);
+}
+
+
+function addNumber(number) {
+    numberDisplay += number.toString();
+    display.innerHTML = numberDisplay;
 }
